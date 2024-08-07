4dbeef77af7ba3401f031a540308fcbb5cf617e3
SamytaS2 <145566706+SamytaS2@users.noreply.github.com>
2024-08-07 16:58:21 -0300

    Animation

--- Oficina24/Assets/Pixel Adventure 1/Assets/Scripts/Player.cs
+++ Oficina24/Assets/Pixel Adventure 1/Assets/Scripts/Player.cs
@@ -11,10 +11,13 @@ public class NewBehaviourScript : MonoBehaviour
     public bool doubleJump;
 
     private Rigidbody2D rig;
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -27,6 +30,21 @@ public class NewBehaviourScript : MonoBehaviour
     void Move(){
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * Speed;
+
+        if(Input.GetAxis("Horizontal") > 0f){
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f,0f,0f);
+        }
+        
+        if(Input.GetAxis("Horizontal") < 0f){
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f,180f,0f);
+        }
+
+        if(Input.GetAxis("Horizontal") == 0f){
+            anim.SetBool("walk", false);
+        }
+
     }
 
     void Jump(){
@@ -34,6 +52,7 @@ public class NewBehaviourScript : MonoBehaviour
             if(!isJumping){
                 rig.AddForce(new Vector2(0f,JumpForce), ForceMode2D.Impulse);
                 doubleJump = true;
+                anim.SetBool("jump", true);
             }
             else{
                 if(doubleJump){
@@ -48,6 +67,7 @@ public class NewBehaviourScript : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision){
         if(collision.gameObject.layer == 8){
             isJumping = false;
+            anim.SetBool("jump", false);
         }
     }
     void OnCollisionExit2D(Collision2D collision){
